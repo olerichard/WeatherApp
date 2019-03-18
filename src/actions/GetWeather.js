bb8de532c802c74f911d688c8fba8e210ca0834f
oboskeboo <post@olerichard.org>
2019-03-18 18:17:31 +0100

    Change homepage for build routine

--- src/actions/GetWeather.js
+++ src/actions/GetWeather.js
@@ -5,7 +5,7 @@ export default async function GetWeather(location) {
   if (location == null)
     return null
 
-  return await axios.get(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${location.latitude}&lon=${location.longitude}&appid=${process.env.REACT_APP_OWM_APIKEY}`)
+  return await axios.get(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${location.latitude}&lon=${location.longitude}&appid=${process.env.REACT_APP_OWM_APIKEY}`)
     .then((res) => res.data)
     .catch(() => console.error("The Weather is a lie"))
 };
